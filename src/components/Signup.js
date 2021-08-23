e9cb42ccbd2729b2a22e7c9db1cd89f29cebfe6d
Hessam <tabatabaee.hessam@gmail.com>
2021-08-24 00:07:12 +1000

    signup component design complete

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
 import { fsDb } from "../services/firebase"
-import { getCurrentUser } from '../helpers/auth';
 
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import { Button, FormLabel, TextField } from '@material-ui/core';
+import Benefit from './Benefit';
+
+
 
 class Signup extends Component {
   constructor() {
@@ -53,62 +51,91 @@ class Signup extends Component {
 
   render(){
     return(
-      <div>
-      <h1>Sign up for our newsletter and 20% off</h1>
-      <p>to use in the Argonauts shop</p>
-      <p>including access to the <span>Cosmos Club</span></p>
-      <form onSubmit={this.handleSubmit}>
-        <div>
-          <input 
-            id="first-name" 
-            type="text" 
-            name="firstName"
-            placeholder="First Name"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div>
-          <input 
-            id="last-name" 
-            type="text" 
-            name="lastName"
-            placeholder="Last Name"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div>
-          <input 
-            id="email" 
-            type="text" 
-            name="email"
-            placeholder="Email"
-            onChange={this.handleChange}
-          />
-        </div>
-        <div>
-          Subscription type:
-          <label>
-            <input
-              type="radio"
-              value="Daily"
-              checked={this.state.subscription === "Daily"}
-              onChange={this.onValChange}/>
-            <span>Daily</span>
-          </label>
+      <div className="signup-container" >
+        <h1 className="signup-title" >Ready to be a Argonaut?</h1>
+        <p className="signup-subtitle" >Take your first step. Be inspired and subscribe below.</p>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input 
+              id="first-name" 
+              type="text" 
+              name="firstName"
+              placeholder="First Name"
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <div>
+            <input 
+              id="last-name" 
+              type="text" 
+              name="lastName"
+              placeholder="Last Name"
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <div>
+            <input 
+              id="email" 
+              type="text" 
+              name="email"
+              placeholder="Email"
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <div>
+            <p className="subscription-title">Subscription type:</p>
+            <div className="signup-subscription" >
+              <label>
+                <div>
+                  Daily
+                </div>
+                <input
+                  type="radio"
+                  value="Daily"
+                  checked={this.state.subscription === "Daily"}
+                  onChange={this.onValChange}/>
+              </label>
 
-          <label>
-            <input
-              type="radio"
-              value="Weekly Digest"
-              checked={this.state.subscription === "Weekly Digest"}
-              onChange={this.onValChange}/>
-            <span>Weekly Digest</span>
-          </label>
-        </div>
-        <div>
-        <button>Confirm</button>
-        </div>
-      </form>
+              <label>
+                <div>
+                  Weekly Digest
+                </div>
+                <input
+                  type="radio"
+                  value="Weekly Digest"
+                  checked={this.state.subscription === "Weekly Digest"}
+                  onChange={this.onValChange}/>
+              </label>
+            </div>
+          </div>
+          <div>
+          </div>
+        </form>
+        <p className="benefit-paragraph" >The benefits start at sign-up.</p>
+        <Benefit 
+          text="20% off to use in the Argonauts Shop, including free delivery." 
+          image="../images/vector.png" 
+        />
+        <Benefit 
+          text="20% off to use in the Argonauts Shop, including free delivery." 
+          image="../images/champagne.png" 
+        />
+        <Benefit 
+          text="20% off to use in the Argonauts Shop, including free delivery." 
+          image="../images/saturn.png" 
+        />
+        <Benefit 
+          text="20% off to use in the Argonauts Shop, including free delivery." 
+          image="../images/cake.png" 
+        />
+        <button className="liftoff-button" onClick={this.handleSubmit} >
+          Lift off
+        </button>
+        <p className="signup-policy-statement" >By clicking this button, you agree to our <strong>Terms of Service</strong> and <strong>Privacy Policy.</strong> </p>
+        <p className="signup-spam-statement" >We promise not to spam you.</p>
       </div>
     )
   }
